3db09fe193f160a86f718b3b71406f28ef7f9fa1
cyan-2048 <72785466+cyan-2048@users.noreply.github.com>
2024-03-03 14:13:12 +0800

    bitching

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import polyfillKaiOS from "./scripts/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: "",
 	plugins: [preact(), polyfillKaiOS()],
 	server: {
 		port: 3000,
